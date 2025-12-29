797e013cb01fbdc497934c983331fba1deb78e84
Jhon Jairo Garzon <jhonjairob119g@gmail.com>
2025-12-29 02:12:15 -0500

    Add variable env de URIS

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,7 @@ import { TypifyModule } from './typify/typify.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://jhonjairo2830b_db_user:dGjgi4KpgRwEirV3@ralle-api.pymddou.mongodb.net/?appName=ralle-api',
-    ),
-    TypifyModule,
-  ],
+  imports: [MongooseModule.forRoot(process.env.MONGO_URI || ''), TypifyModule],
   controllers: [AppController],
   providers: [AppService],
 })
